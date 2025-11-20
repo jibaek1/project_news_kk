417c62764e6adb1c1340b5e723536885163bb21d
jibaek1 <tngus000777@gmail.com>
2025-11-20 12:24:59 +0900

    News 도메인 뼈대 및 Jsoup의존성 추가

--- src/main/java/com/ssr/newskuku/domain/news/News.java
+++ src/main/java/com/ssr/newskuku/domain/news/News.java
@@ -1,4 +1,25 @@
 package com.ssr.newskuku.domain.news;
 
-public class News {
+
+import com.ssr.newskuku._global.entity.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.ibatis.type.Alias;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Alias("News")
+public class News extends BaseEntity {
+
+    private Long newsId;
+    private String title;
+    private String content;
+    private String url;
+    private int categoryId;
+    private String thumbNail;
+
 }
