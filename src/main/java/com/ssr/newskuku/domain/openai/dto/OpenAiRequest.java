f11d2c571ae100a8f5aa6db586fb366e76bec421
seongyob99 <seongyob99@naver.com>
2025-11-20 15:36:14 +0900

    OpenAI API 연동 완료

---
+++ src/main/java/com/ssr/newskuku/domain/openai/dto/OpenAiRequest.java
@@ -0,0 +1,24 @@
+package com.ssr.newskuku.domain.openai.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OpenAiRequest {
+    private String model;
+    private List<Message> messages;
+    private double temperature;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Message {
+        private String role;
+        private String content;
+    }
+}
